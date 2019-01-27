e7a7e62e991ecddc4e5464a82884e45def989938
Anwesh Budhathoki <45763486+abperni@users.noreply.github.com>
2019-01-27 22:48:45 +0545

    Update WAP to print basic pyramid with input.c

--- Pyramids/WAP to print basic pyramid with input.c
+++ Pyramids/WAP to print basic pyramid with input.c
@@ -1,17 +1,19 @@
+//WAP to print a normal pyramid with an input from user 
+
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-	int a,b,n;
+	int a,b,n;	//taking 3 integer variables a and b for for loop and n for the input from user
 	printf("Enter any number:\n");
-	scanf("%d",&n);
-	for(a=1;a<=n;++a)
+	scanf("%d",&n);		//take the maximum block of pyramid
+	for(a=1;a<=n;++a)	//determining the no of rows upto n 
 	{
-		printf("\n");
-		for(b=1;b<=a;b++)
+		printf("\n");	//goes to next line
+		for(b=1;b<=a;b++)	//determing the value of column upto the value of row
 		{
-			printf("%d",b);
+			printf("%d",b);	//printing the value of b
 		}
 	}
-	getch();
+	getch();	//hold the screen
 }
